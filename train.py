09fc5cba4272dcfe93d34f89a668722335c7c7d1
Anh <duyanhbrocode@gmail.com>
2025-03-04 15:36:29 +0700

    train

--- train.py
+++ train.py
@@ -129,5 +129,5 @@ def main():
 
 if __name__ == "__main__":
     if opt.preprocess:
-        prepare_data(data_path="data", patch_size=40 if opt.mode == "S" else 50, stride=10, aug_times=1 if opt.mode == "S" else 2)
+        prepare_data(data_path="/kaggle/working/train", patch_size=40 if opt.mode == "S" else 50, stride=10, aug_times=1 if opt.mode == "S" else 2)
     main()
